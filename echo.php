e3046ec931bc6f2a1c843c58811e0e7fe3ecb00e
Mark Kremer <mark@socialreferral.com>
2011-10-30 12:24:47 +0100

    Added ReverseEcho call

--- echo.php
+++ echo.php
@@ -3,14 +3,23 @@
   if (count($argv) > 1) {
     $message = $argv[1];
   } else {
-    $message = "I have nothing to say";
+    $message = "Hello from PHP";
   }
 
   $wsdl = "http://localhost:9292/echo_service.wsdl";
   $client = new SoapClient($wsdl, array("trace" => 1));
 
   $response = $client->Echo(array("Message" => $message));
-  echo "The Echo service returned: " . $response->Message . "\n\n";
+  echo "EchoService responded to Echo: " . $response->Message . "\n\n";
+  echo "The client posted the following XML message:\n";
+  echo $client->__getLastRequest();
+  echo "\n";
+  echo "The client received the following XML message in response:\n";
+  echo $client->__getLastResponse();
+  echo "\n";
+
+  $response = $client->ReverseEcho(array("Message" => $message));
+  echo "EchoService responded to ReverseEcho: " . $response->Message . "\n\n";
   echo "The client posted the following XML message:\n";
   echo $client->__getLastRequest();
   echo "\n";
